c37a893fd2211eab4c7c8e21fa5ad8119a92ec95
M.Gharouni <mmahdi.gjafari69@gmail.com>
2023-10-10 16:59:04 +0330

    imp api & data part

---
+++ app/src/main/java/com/plcoding/weatherapp/data/remote/WeatherDataDto.kt
@@ -0,0 +1,25 @@
+package com.plcoding.weatherapp.data.remote
+
+import com.squareup.moshi.Json
+
+data class WeatherDataDto(
+
+    val time: List<String>,
+
+    @field:Json(name = "temperature_2m")
+    val temperature: List<Double>,
+
+    @field:Json(name = "weathercode")
+    val weatherCode: List<Int>,
+
+    @field:Json(name = "relativehumidity_2m")
+    val humidity: List<Double>,
+
+    @field:Json(name = "windspeed_10m")
+    val windSpeed: List<Double>,
+
+    @field:Json(name = "pressure_msl")
+    val pressure: List<Double>,
+
+
+    )
